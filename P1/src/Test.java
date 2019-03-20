db7fbd448c278689fd5f6bb37a22f6151f6c9fa2
Tiago Fragoso <tiagofragoso@outlook.com>
2019-03-20 22:27:47 +0000

    Created a sample class

---
+++ P1/src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public void doSomething() {
+        System.out.println("This is a test class");
+    }
+
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.doSomething();
+    }
+}
+
+
